0b79484db641d5ca85a28c662f126e2b38f9d777
lango <xmun777@naver.com>
2023-01-30 23:20:23 +0900

    feat: project init and git ignore update

---
+++ src/main/java/com/kakao/saramaracommunity/SaramaraCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.saramaracommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaramaraCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaramaraCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/saramaracommunity/SaramaraCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.saramaracommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaramaraCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
