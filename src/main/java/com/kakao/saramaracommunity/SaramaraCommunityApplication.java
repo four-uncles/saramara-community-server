06f736f53aedd8d8069ec83d69e04016877e9515
lango <xmun777@naver.com>
2023-02-01 23:57:10 +0900

    feat: JPA Auditing filed apply (createdAt, updatedAt, deletedAt)

--- src/main/java/com/kakao/saramaracommunity/SaramaraCommunityApplication.java
+++ src/main/java/com/kakao/saramaracommunity/SaramaraCommunityApplication.java
@@ -2,7 +2,9 @@ package com.kakao.saramaracommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SaramaraCommunityApplication {
 
