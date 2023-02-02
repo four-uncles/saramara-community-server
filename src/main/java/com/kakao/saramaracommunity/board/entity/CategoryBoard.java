58ffa55f05cde6ac8faad6f5ab43cd315e24e80f
IToriginal <gitshineit@gmail.com>
2023-02-02 22:10:05 +0900

    feat: BoardRepoositoryTest Sample Data insert Test Code

---
+++ src/main/java/com/kakao/saramaracommunity/board/entity/CategoryBoard.java
@@ -0,0 +1,5 @@
+package com.kakao.saramaracommunity.board.entity;
+
+public enum CategoryBoard {
+    NORMAL, QUESTION
+}
---
+++ src/test/java/com/kakao/saramaracommunity/board/repository/BoardRepoositoryTest.java
@@ -0,0 +1,33 @@
+package com.kakao.saramaracommunity.board.repository;
+
+import com.kakao.saramaracommunity.board.entity.Board;
+import com.kakao.saramaracommunity.board.entity.CategoryBoard;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.LongStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepoositoryTest {
+    @Autowired
+    private BoardRepository boardRepository;
+    @Test
+    public void 게시판등록() {
+        LongStream.rangeClosed(1, 100).forEach(i->{
+            Board board = Board.builder()
+                    .memberId(i)
+                    .title("Sample 제목[" + i + "]")
+                    .content("Sample 내용[" + i + "]")
+                    .like((long)(Math.random() * 100) + 1)
+                    .build();
+            board.addCate(CategoryBoard.QUESTION);
+            if (i > 90) {
+                board.addCate(CategoryBoard.NORMAL);
+            }
+            boardRepository.save(board);
+        });
+    }
+}
