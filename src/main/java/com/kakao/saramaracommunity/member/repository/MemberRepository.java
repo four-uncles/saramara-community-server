ab4358e43151ddf35cddfc65c8be30d7208f244a
lango <xmun777@naver.com>
2023-02-06 21:09:52 +0900

    feat: MemberRepositoryTest test code apply

--- src/main/java/com/kakao/saramaracommunity/member/repository/MemberRepository.java
+++ src/main/java/com/kakao/saramaracommunity/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.kakao.saramaracommunity.member.repository;
 import com.kakao.saramaracommunity.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
 }
--- src/test/java/com/kakao/saramaracommunity/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/kakao/saramaracommunity/member/repository/MemberRepositoryTest.java
@@ -3,11 +3,13 @@ package com.kakao.saramaracommunity.member.repository;
 import com.kakao.saramaracommunity.member.entity.Member;
 import com.kakao.saramaracommunity.member.entity.Role;
 import com.kakao.saramaracommunity.member.entity.Type;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,8 +19,12 @@ public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @AfterEach
+    public void clean() {
+        memberRepository.deleteAll();
+    }
     @Test
-    public void 사용자_등록() {
+    public void save() {
         // given
         memberRepository.save(Member.builder()
                 .type(Type.LOCAL)
@@ -39,7 +45,7 @@ public class MemberRepositoryTest {
     }
 
     @Test
-    public void 사용자_삭제() {
+    public void deleteById() {
         // soft delete 방식으로 삭제되어야 한다.
         // given
         Member member1 = memberRepository.save(Member.builder()
@@ -74,7 +80,7 @@ public class MemberRepositoryTest {
     }
 
     @Test
-    public void 사용자_조회() {
+    public void findAll() {
         //given
         Member member = memberRepository.save(Member.builder()
                 .type(Type.LOCAL)
@@ -84,13 +90,88 @@ public class MemberRepositoryTest {
                 .role(Role.BASIC)
                 .build());
         memberRepository.save(member);
+
         //when
         List<Member> memberList = memberRepository.findAll();
 
         //then
-        Member lango = memberList.get(0);
-        assertThat(lango.getMemberId()).isEqualTo(member.getMemberId());
-        assertThat(lango.getEmail()).isEqualTo(member.getEmail());
-        assertThat(lango.getNickname()).isEqualTo(member.getNickname());
+        Member result = memberList.get(0);
+        assertThat(result.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(result.getEmail()).isEqualTo(member.getEmail());
+        assertThat(result.getNickname()).isEqualTo(member.getNickname());
+    }
+
+    @Test
+    public void findById() {
+        //given
+        Member member = memberRepository.save(Member.builder()
+                .type(Type.LOCAL)
+                .email("lango@kakao.com")
+                .nickname("lango")
+                .password("kakao")
+                .role(Role.BASIC)
+                .build());
+        memberRepository.save(member);
+
+        //when
+        Optional<Member> optional = memberRepository.findById(member.getMemberId());
+        Member result = optional.get();
+
+        //then
+        assertThat(result.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(result.getEmail()).isEqualTo(member.getEmail());
+        assertThat(result.getNickname()).isEqualTo(member.getNickname());
+    }
+
+    @Test
+    public void findByEmail() {
+        //given
+        Member member = memberRepository.save(Member.builder()
+                .type(Type.LOCAL)
+                .email("lango@kakao.com")
+                .nickname("lango")
+                .password("kakao")
+                .role(Role.BASIC)
+                .build());
+        memberRepository.save(member);
+
+        //when
+        Optional<Member> optional = memberRepository.findByEmail(member.getEmail());
+        Member result = optional.get();
+
+        //then
+        assertThat(result.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(result.getEmail()).isEqualTo(member.getEmail());
+        assertThat(result.getNickname()).isEqualTo(member.getNickname());
+    }
+
+    @Test
+    public void update() {
+        //given
+        Member member = memberRepository.save(Member.builder()
+                .type(Type.LOCAL)
+                .email("lango@kakao.com")
+                .nickname("lango")
+                .password("kakao")
+                .role(Role.BASIC)
+                .build());
+        memberRepository.save(member);
+
+        Member updateMember = Member.builder()
+                .type(member.getType())
+                .email("lango@naver.com")
+                .nickname(member.getNickname())
+                .password(member.getPassword())
+                .role(member.getRole())
+                .build();
+        memberRepository.save(updateMember);
+        //when
+        List<Member> memberList = memberRepository.findAll();
+        Member result = memberList.get(0);
+
+        //then
+        assertThat(result.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(result.getEmail()).isEqualTo(member.getEmail());
+        assertThat(result.getNickname()).isEqualTo(member.getNickname());
     }
 }
