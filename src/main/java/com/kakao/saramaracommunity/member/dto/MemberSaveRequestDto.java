96973e60acb4280201bf82ed0e80debbfe2c6f77
lango <xmun777@naver.com>
2023-02-19 22:47:17 +0900

    feat: Member register api develop apply [service, dto, test]

---
+++ src/main/java/com/kakao/saramaracommunity/member/dto/MemberSaveRequestDto.java
@@ -0,0 +1,40 @@
+package com.kakao.saramaracommunity.member.dto;
+
+import com.kakao.saramaracommunity.member.entity.Member;
+import com.kakao.saramaracommunity.member.entity.Role;
+import com.kakao.saramaracommunity.member.entity.Type;
+import lombok.*;
+
+
+@NoArgsConstructor
+@ToString
+@Getter
+public class MemberSaveRequestDto {
+    private Type type;
+    private String email;
+    private String nickname;
+    private String password;
+    private Role role;
+    private String picture;
+
+    @Builder
+    public MemberSaveRequestDto(Type type, String email, String nickname, String password, Role role, String picture) {
+        this.type = type;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.role = role;
+        this.picture = picture;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .type(type)
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .role(role)
+                .picture(picture)
+                .build();
+    }
+}
