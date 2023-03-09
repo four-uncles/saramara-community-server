4ca7d50b7d27d7766fa0126fefac8cbb5c2528b3
lango <xmun777@naver.com>
2023-03-09 23:05:29 +0900

    feat: MemberController with Test apply

--- src/main/java/com/kakao/saramaracommunity/member/controller/MemberController.java
+++ src/main/java/com/kakao/saramaracommunity/member/controller/MemberController.java
@@ -1,26 +1,32 @@
 package com.kakao.saramaracommunity.member.controller;
 
-import com.kakao.saramaracommunity.auth.dto.OAuthAttributes;
-import com.nimbusds.oauth2.sdk.util.StringUtils;
-import jakarta.servlet.http.HttpServletResponse;
+import com.kakao.saramaracommunity.member.dto.MemberSaveRequestDto;
+import com.kakao.saramaracommunity.member.service.MemberSerivce;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @Log4j2
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
+    private final MemberSerivce memberSerivce;
+
     @GetMapping("/")
     public ResponseEntity<String> index(Authentication authentication) {
         System.out.println("authentication: " + authentication);
         return ResponseEntity.ok().body(String.valueOf(authentication));
-//        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/api/register")
+    public ResponseEntity<Long> register(@RequestBody MemberSaveRequestDto requestDto) {
+        Long memberId = memberSerivce.register(requestDto);
+        return ResponseEntity.ok().body(memberId);
     }
 }
