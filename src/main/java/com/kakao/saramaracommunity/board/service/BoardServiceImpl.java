37e58d5c3db017485c869ee8470e9bedb0b5bd0a
IToriginal <gitshineit@gmail.com>
2023-06-21 00:22:42 +0900

    :hammer: fix: BoardService 및 BoardServiceImpl 코드 수정
    - DTOtoEntity 주석처리: ModelMaper 도입을 고려
    - BoardPageResponseDTO 요청 메서드 삭제

--- src/main/java/com/kakao/saramaracommunity/board/service/BoardServiceImpl.java
+++ src/main/java/com/kakao/saramaracommunity/board/service/BoardServiceImpl.java
@@ -20,25 +20,8 @@ import java.util.function.Function;
 public class BoardServiceImpl implements BoardService {
     private final BoardRepository boardRepository;
 
-    // Board Register
     @Override
     public Long boardRegister(BoardDTO boardDTO) {
-        log.info("Service: " + boardDTO);
-        Board board = dtoToEntity(boardDTO);
-        boardRepository.save(board);
-        return board.getId();
-    }
-
-    @Override
-    public BoardPageResponseDTO<BoardDTO, Object[]> getBoardList(BoardPageRequestDTO boardPageRequestDTO) {
-        log.info("board: " + boardPageRequestDTO);
-
-        Function<Object [], BoardDTO> fn = (ent -> entityToDTO(
-                (Board)ent[0], (Member)ent[1], (Long)ent[2]));
-
-        Page<Object []> result = boardRepository.getBoardWithCommentCount(
-                boardPageRequestDTO.getPageable(
-                        Sort.by("boardId").descending()));
-        return new BoardPageResponseDTO<>(result, fn);
+        return null;
     }
 }
