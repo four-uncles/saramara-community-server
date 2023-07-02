6b542e287ef720db45438052756c47dabee810d6
hb9397 <hb9397@naver.com>
2023-07-03 00:20:50 +0900

    :bug: Fix: GlobalExceptionHandler 클래스 분리 - ValidExceptionHandlingMethod 클래스에 Dto에 @Valid로 검증되는 필드에 대해 다루는 로직을 메서드 단위로 분리하여 수정
    - nickname에 대한 검증 로직 추가 필요
    
    - 위를 위해 작성된 코드 주석

--- src/main/java/com/kakao/saramaracommunity/member/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/saramaracommunity/member/exception/GlobalExceptionHandler.java
@@ -2,103 +2,72 @@ package com.kakao.saramaracommunity.member.exception;
 
 import java.util.List;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import com.kakao.saramaracommunity.member.dto.MemberResDto;
 import com.kakao.saramaracommunity.member.dto.ErrorCode;
-import com.kakao.saramaracommunity.member.service.MemberServiceMethod;
-
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
+/*
+@Valid 어노테이션의 유효성을 통과하지 못하는 경우 MethodArgumentNotValidException 을 발생하므로
+@ExceptionHandler(MethodArgumentNotValidException.class) 으로 @Valid의 Exception을 처리하겠다고 선언
+Exception에 대한 결과를 ErrorCode 클래스를 이용해서 MemberResDto를 통해 Client에 응답하기 위함이다.
+*/
 @Log4j2
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<MemberResDto> handleMethodArgumentNotValidException(
-		MethodArgumentNotValidException exception, HttpServletRequest request) {
+	public ResponseEntity<MemberResDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+		ValidExceptionHandlingMethod validExceptionHandlingMethod = new ValidExceptionHandlingMethod();
 
+		/*
+		@Valid로 부터 발생되는 MethodArgumentNotValidException 을 exception 이라는 객체로 받아와
+		Binding 및 유효성 검증 결과를 저장하는 인터페이스 BindingResult 형의 객체에 해당 Exception 검증결과를 getBindingResult()로 받아와
+		유혀성 검증에서 발생된 Exception 을 FieldError 형태의 List로 가지고 있어서 해당되는 Exception에 접근할 수 있도록 했다.
+		*/
 		BindingResult bindingResult = exception.getBindingResult();
 		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 
-		ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
-		String errorMessage;
-		HttpStatus status;
+		MemberResDto response = MemberResDto.builder()
+			.success(false)
+			.status(ErrorCode.INTERNAL_SERVER_ERROR)
+			.build();
 
-		log.error(fieldErrors);
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
+		// MethodArgumentNotValidException 에 대한 Exception 이 감지된 경우를 의미
 		if (!fieldErrors.isEmpty()) {
 			FieldError fieldError = fieldErrors.get(0);
 			String fieldErrorCode = fieldError.getCode();
 
+			// @Valid Exception 의 필드가 이메일인 경우
 			if (fieldError.getField().equals("email")) {
-				errorCode = ErrorCode.INVALID_INPUT_EMAIL;
-				log.warn(fieldError.getCode());
-				if (fieldErrorCode.equals("NotNull") || fieldErrorCode.equals("NotBlank")) {
-					errorMessage = ErrorCode.INVALID_INPUT_EMAIL.getDescription();
-					status = ErrorCode.INVALID_INPUT_EMAIL.getHttpStatus();
-				} else if (fieldErrorCode.equals("Size")) {
-					errorMessage = fieldError.getDefaultMessage();
-					status = ErrorCode.INVALID_INPUT_VALUE.getHttpStatus();
-				} else if (fieldErrorCode.equals("Pattern")) {
-					log.warn(fieldErrorCode.equals("Pattern"));
-					errorMessage = ErrorCode.INVALID_INPUT_EMAIL.getDescription();
-					status = ErrorCode.INVALID_INPUT_EMAIL.getHttpStatus();
-				} else {
-					errorMessage = ErrorCode.INTERNAL_SERVER_ERROR.getDescription();
-					status = ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus();
-				}
-			} else if (fieldError.getField().equals("password")) {
-				errorCode = ErrorCode.INVALID_INPUT_PW;
-				log.warn(fieldError.getCode());
-				if (fieldErrorCode.equals("NotNull") || fieldErrorCode.equals("NotBlank")) {
-					errorMessage = ErrorCode.INVALID_INPUT_PW.getDescription();
-					status = ErrorCode.INVALID_INPUT_PW.getHttpStatus();
-				} else if (fieldErrorCode.equals("Pattern")) {
-					errorMessage = ErrorCode.INVALID_INPUT_PW.getDescription();
-					status = ErrorCode.INVALID_INPUT_PW.getHttpStatus();
-				} else {
-					errorMessage = ErrorCode.INTERNAL_SERVER_ERROR.getDescription();
-					status = ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus();
-				}
-			} else {
-				errorMessage = ErrorCode.INVALID_INPUT_VALUE.getDescription();
-				status = ErrorCode.INVALID_INPUT_VALUE.getHttpStatus();
+				response = validExceptionHandlingMethod.emailValidExceptionHandling(fieldError, fieldErrorCode);
+
+				// response 는 MemberResDto로 해당 클래스의 status는 ErrorCode 클래스의 정의된 status를 의미하며 이를 Getter 로 가져와서
+				// ErrorCode에 정의된 getHttpStatus() 메서드로 response(현재 Exception에 상응하는 HttpStatus를 가져온다.
+				status = response.getStatus().getHttpStatus();
+
+				return new ResponseEntity<>(response, status);
 			}
+			// @Valid Exception 의 필드가 비밀번호인 경우
+			else if (fieldError.getField().equals("password")) {
+				response = validExceptionHandlingMethod.passwordValidExceptionHandling(fieldErrorCode);
+				status = response.getStatus().getHttpStatus();
 
-			MemberResDto response = MemberResDto.builder()
-				.success(false)
-				.status(errorCode)
-				.data(errorMessage)
-				.build();
-
-			return new ResponseEntity<>(response, headers, status);
+				return new ResponseEntity<>(response, status);
+			}
 		}
 
-		errorMessage = ErrorCode.INTERNAL_SERVER_ERROR.getDescription();
-		status = ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus();
-		MemberResDto response = MemberResDto.builder()
-			.success(false)
-			.status(ErrorCode.INTERNAL_SERVER_ERROR)
-			.data(errorMessage)
-			.build();
 
-		return new ResponseEntity<>(response, headers, status);
+		return new ResponseEntity<>(response, status);
 	}
 }
+
