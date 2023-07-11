f1112d8c52a69dc6748cdd27f991ec8e85959ddc
IToriginal <gitshineit@gmail.com>
2023-07-11 15:07:30 +0900

    :sparkles: feat(#40): Repository 메서드 생성
    게시글 전체를 조회하기 위한 method
    - 1. 최신순
    - 2. 인기순

--- src/main/java/com/kakao/saramaracommunity/board/repository/BoardRepository.java
+++ src/main/java/com/kakao/saramaracommunity/board/repository/BoardRepository.java
@@ -1,11 +1,19 @@
 package com.kakao.saramaracommunity.board.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.kakao.saramaracommunity.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Optional<Board> findByBoardIdAndDeletedAtIsNull(Long boardId);
+
+    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL ORDER BY b.createdAt DESC")
+    List<Board> findAllByDeletedAtIsNullOrderByCreatedAtDesc();
+
+    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL ORDER BY b.likeCnt DESC")
+    List<Board> findAllByDeletedAtIsNullOrderByLikeCntDesc();
 }
