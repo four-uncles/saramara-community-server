ae0fe42c5478326c1917e44b3b3643dcfdd8c27a
hb9397 <hb9397@naver.com>
2023-07-17 00:13:44 +0900

    :bug: Fix: Member Controller (MethodArgumentNotValidException 을 PathVariable 로 받는 값이 발생시키도록 수정) - 회원관련 Controller 에서 PathVariable 로 받는 회원정보에 대해서 @Pattern 으로 정규식 추가해서, MethodArgumentNotValidException 을 발생시키도록 추가.

--- src/main/java/com/kakao/saramaracommunity/member/controller/MemberController.java
+++ src/main/java/com/kakao/saramaracommunity/member/controller/MemberController.java
@@ -52,7 +52,7 @@ public class MemberController {
     // 회원가입 - 이메일 중복확인
     @GetMapping("/member/email/{email}")
     public ResponseEntity<MemberResDto> isDuplicateEmail(
-        @Valid @PathVariable String email
+        @Valid @PathVariable @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$") String email
     ){
         MemberResDto response = signUpCheckingService.checkingDuplicateEmail(email);
         HttpStatus status = memberServiceMethod.changeStatus(response);
@@ -62,7 +62,7 @@ public class MemberController {
     // 회원가입 - 닉네임 중복확인
     @GetMapping("/member/nickname/{nickname}")
     public ResponseEntity<MemberResDto> isDuplicatedNickname(
-        @Valid @PathVariable String nickname
+        @Valid @PathVariable @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$") String nickname
     ){
         MemberResDto response = signUpCheckingService.checkingDuplicatedNickName(nickname);
         HttpStatus status = memberServiceMethod.changeStatus(response);
@@ -83,8 +83,8 @@ public class MemberController {
     @PutMapping("/member/nickname/{email}/{currentNickname}/{changeNickname}")
     public ResponseEntity<MemberResDto> nicknameChanging(
         @Valid @PathVariable @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")String email,
-        @Valid @PathVariable @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$")String currentNickname,
-        @Valid @PathVariable @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$")String changeNickname
+        @Valid @PathVariable @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$") String currentNickname,
+        @Valid @PathVariable @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$") String changeNickname
         ){
         MemberResDto response = memberSerivce.nickNameChange(email, currentNickname, changeNickname);
         HttpStatus status = memberServiceMethod.changeStatus(response);
@@ -94,17 +94,18 @@ public class MemberController {
     // 비밀번호 수정
     @PutMapping("/member/password/{email}")
     public ResponseEntity<MemberResDto> passwordChange(
-        @Valid @PathVariable String email,
-        @Valid @RequestBody ChangePWDto changePWDto
+        @Valid @PathVariable @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$") String email,
+        @Valid @RequestBody @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}") ChangePWDto changePWDto
     ){
         MemberResDto response = memberSerivce.passwordChange(email, changePWDto);
         HttpStatus status = memberServiceMethod.changeStatus(response);
         return new ResponseEntity<>(response, status);
     }
 
+    // 회원탈퇴
     @DeleteMapping("/member/{email}")
     public ResponseEntity<MemberResDto> unregisterMember(
-        @Valid @PathVariable String email
+        @Valid @PathVariable @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$") String email
     ){
         MemberResDto response = memberSerivce.unregister(email);
         HttpStatus status = memberServiceMethod.changeStatus(response);
