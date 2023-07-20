103e20d0c42c01f686cb1fea67e9de6abf51afb0
IToriginal <gitshineit@gmail.com>
2023-07-21 01:17:32 +0900

    :sparkles: feat(#50): Controller 구현
    PatchMapping
    - 수정이 완료되면 Boolean을 통해 성공여부만 응답한다.

--- src/main/java/com/kakao/saramaracommunity/board/controller/BoardController.java
+++ src/main/java/com/kakao/saramaracommunity/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -85,5 +86,20 @@ public class BoardController {
         response.put("data", boards);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{boardId}")
+    public ResponseEntity<Object> updateBoard(@PathVariable ("boardId") Long boardId,
+        @RequestBody @Valid BoardRequestDto.UpdateRequestDto requestDto) {
+
+        Boolean result = boardService.updateBoard(boardId, requestDto);
+
+        // 응답 데이터 생성
+        Map<String, Object> response = new HashMap<>();
+        response.put("code", 200);
+        response.put("msg", "success");
+        response.put("result", result);
+
+        return ResponseEntity.status((int)response.get("code")).body(response);
     }
 }
