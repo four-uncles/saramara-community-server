bbd9e616d7f6374a3a52fa35e70ea49fe8ded8bb
lango <xmun777@naver.com>
2023-08-23 00:38:02 +0900

    hotfix: handleMethodArgumentNotValidException fix - ResponseEntityExceptionHandler의 handleMethodArgumentNotValid 메서드를 재정의했을 때으 시그니처 미수정으로 인한 문제 해결

--- src/main/java/com/kakao/saramaracommunity/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/saramaracommunity/common/exception/GlobalExceptionHandler.java
@@ -5,14 +5,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
@@ -33,16 +30,14 @@ public class GlobalExceptionHandler {
      * 유효성 검사를 진행할 @Valid 어노테이션이 붙은 요청 DTO 클래스에서 유효성 검사가 실패할 경우 발생하는 예외를 핸들링하는 메서드입니다.
      * 메시지의 경우 유효성 검사 결과 객체에서 필드 오류 정보를 찾은 후, 해당 예외의 기본 메시지를 추출하여 문자열로 반환합니다.
      *
+     * ResponseEntity를 사용한 이유는 응답 상태 코드, 헤더 등을 더 세밀하게 제어할 수 있기 때문입니다.
+     * 또한, HTTP 응답을 더 세부적으로 커스터마이징할 수 있기 때문에 유연성이 높습니다.
+     *
      * @param exception 유효성 검증 실패 시 던지는 예외
      * @return 예외 메세지와 응답 코드를 리턴
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Payload> handleMethodArgumentNotValidException (
-            MethodArgumentNotValidException exception,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request
-    ) {
+    protected ResponseEntity<Payload> handleMethodArgumentNotValidException (MethodArgumentNotValidException exception) {
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
