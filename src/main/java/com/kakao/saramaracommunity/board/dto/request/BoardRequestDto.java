ed0d289039dc95c16d9033b7d0dbdcb9948fcc4c
IToriginal <gitshineit@gmail.com>
2023-08-22 23:23:42 +0900

    :hammer: fix(#62): @Valid 예외 추가
    deadline의 설정을 현재시간 보다 과거로 지정할 경우, 예외처리
    
    java.validation 어노테이션
    @Future: 현재 보다 미래인지 확인하는 유효성 검사

--- src/main/java/com/kakao/saramaracommunity/board/dto/request/BoardRequestDto.java
+++ src/main/java/com/kakao/saramaracommunity/board/dto/request/BoardRequestDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kakao.saramaracommunity.board.entity.CategoryBoard;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -26,13 +27,14 @@ public class BoardRequestDto {
         @NotBlank(message = "내용을 입력해주세요.")
         private String content;
 
-        @NotNull(message = "카테고리를 선택해주세요.")
+        @NotNull(message = "카테고리가 선택되지 않았습니다.")
         private CategoryBoard categoryBoard;
 
         @NotNull(message = "사용자의 고유번호가 존재하지 않습니다.")
         private Long memberId;
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @Future(message = "유효 기간은 현재 시간 이후로 설정해야 합니다.")
         private LocalDateTime deadLine;
     }
 
@@ -41,6 +43,7 @@ public class BoardRequestDto {
     @Getter
     @Builder
     public static class UpdateRequestDto {
+        private Long memberId;
 
         @NotBlank(message = "제목을 입력해주세요.")
         private String title;
@@ -48,10 +51,11 @@ public class BoardRequestDto {
         @NotBlank(message = "내용을 입력해주세요.")
         private String content;
 
-        @NotNull(message = "카테고리를 선택해주세요.")
+        @NotNull(message = "카테고리가 선택되지 않았습니다.")
         private CategoryBoard categoryBoard;
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @Future(message = "유효 기간은 현재 시간 이후로 설정해야 합니다.")
         private LocalDateTime deadLine;
     }
 }
