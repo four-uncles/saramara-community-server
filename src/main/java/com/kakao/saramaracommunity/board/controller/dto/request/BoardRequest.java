98481bd0ea7f7a79a7968c62aeadb5ed3d59e117
langoustinee <xmun777@naver.com>
2023-11-06 21:17:02 +0900

    refactor: 게시글(AttachBoard) 이미지 관련 로직 추가 1. 게시글(1):게시글이미지(N)의 관계를 가지도록 엔티티 개발 2. 요청 DTO에 게시글 이미지 목록을 받도록 수정

--- src/main/java/com/kakao/saramaracommunity/board/controller/dto/request/BoardRequest.java
+++ src/main/java/com/kakao/saramaracommunity/board/controller/dto/request/BoardRequest.java
@@ -1,21 +1,22 @@
 package com.kakao.saramaracommunity.board.controller.dto.request;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kakao.saramaracommunity.board.entity.CategoryBoard;
 
-import com.kakao.saramaracommunity.board.service.dto.request.BoardServiceRequestDto;
+import com.kakao.saramaracommunity.board.service.dto.request.BoardServiceRequest;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-public class BoardRequestDto {
+public class BoardRequest {
 
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Getter
-    public static class SaveRequestDto {
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class BoardCreateRequest {
 
         @NotBlank(message = "제목을 입력해주세요.")
         private String title;
@@ -33,22 +34,33 @@ public class BoardRequestDto {
         @Future(message = "유효 기간은 현재 시간 이후로 설정해야 합니다.")
         private LocalDateTime deadLine;
 
+        private List<String> boardImages;
+
         @Builder
-        private SaveRequestDto(String title, String content, CategoryBoard categoryBoard, Long memberId, LocalDateTime deadLine) {
+        private BoardCreateRequest(
+                String title,
+                String content,
+                CategoryBoard categoryBoard,
+                Long memberId,
+                LocalDateTime deadLine,
+                List<String> boardImages
+        ) {
             this.title = title;
             this.content = content;
             this.categoryBoard = categoryBoard;
             this.memberId = memberId;
             this.deadLine = deadLine;
+            this.boardImages = boardImages;
         }
 
-        public BoardServiceRequestDto.SaveRequestDto toServiceRequest() {
-            return BoardServiceRequestDto.SaveRequestDto.builder()
+        public BoardServiceRequest.BoardCreateServiceRequest toServiceRequest() {
+            return BoardServiceRequest.BoardCreateServiceRequest.builder()
                     .title(title)
                     .content(content)
                     .categoryBoard(categoryBoard)
                     .memberId(memberId)
                     .deadLine(deadLine)
+                    .boardImages(boardImages)
                     .build();
         }
 
@@ -56,8 +68,9 @@ public class BoardRequestDto {
 
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Getter
-    public static class UpdateRequestDto {
+    public static class BoardUpdateRequest {
 
+        @NotNull(message = "사용자의 고유번호가 존재하지 않습니다.")
         private Long memberId;
 
         @NotBlank(message = "제목을 입력해주세요.")
@@ -73,24 +86,34 @@ public class BoardRequestDto {
         @Future(message = "유효 기간은 현재 시간 이후로 설정해야 합니다.")
         private LocalDateTime deadLine;
 
+        private List<String> boardImages;
+
         @Builder
-        private UpdateRequestDto(Long memberId, String title, String content, CategoryBoard categoryBoard, LocalDateTime deadLine) {
+        private BoardUpdateRequest(
+                Long memberId,
+                String title,
+                String content,
+                CategoryBoard categoryBoard,
+                LocalDateTime deadLine,
+                List<String> boardImages
+        ) {
             this.memberId = memberId;
             this.title = title;
             this.content = content;
             this.categoryBoard = categoryBoard;
             this.deadLine = deadLine;
+            this.boardImages = boardImages;
         }
 
-        public BoardServiceRequestDto.UpdateRequestDto toServiceRequest() {
-            return BoardServiceRequestDto.UpdateRequestDto.builder()
+        public BoardServiceRequest.BoardUpdateServiceRequest toServiceRequest() {
+            return BoardServiceRequest.BoardUpdateServiceRequest.builder()
                     .memberId(memberId)
                     .title(title)
                     .content(content)
                     .categoryBoard(categoryBoard)
                     .deadLine(deadLine)
+                    .boardImages(boardImages)
                     .build();
         }
-
     }
 }
