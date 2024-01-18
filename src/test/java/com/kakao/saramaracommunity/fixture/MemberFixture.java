c20f3ae38ea9145ec70c82fe3cbbf20b3e376d8f
langoustinee <xmun777@naver.com>
2024-01-18 20:04:06 +0900

    fix: 게시글 및 회원 테스트 픽스처 추가하기

--- src/test/java/com/kakao/saramaracommunity/fixture/MemberFixture.java
+++ src/test/java/com/kakao/saramaracommunity/fixture/MemberFixture.java
@@ -2,13 +2,45 @@ package com.kakao.saramaracommunity.fixture;
 
 import com.kakao.saramaracommunity.member.entity.Member;
 
-public class MemberFixture {
+public enum MemberFixture {
 
-    public static Member createMember(String email, String nickname) {
+    NORMAL_MEMBER_LANGO(
+            "lango@gmail.com",
+            "langopwd1234",
+            "lango"
+    ),
+    NORMAL_MEMBER_SONNY(
+            "sonny@gmail.com",
+            "sonnypwd1288",
+            "sonny"
+    ),
+    ;
+
+    private final String email;
+    private final String password;
+    private final String nickname;
+
+    MemberFixture(
+            String email,
+            String password,
+            String nickname
+    ) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public String getEmail() { return email; }
+
+    public String getPassword() { return password; }
+
+    public String getNickname() { return nickname; }
+
+    public Member createMember() {
         return Member.builder()
-                .email(email)
-                .password("test")
-                .nickname(nickname)
+                .email(getEmail())
+                .password(getPassword())
+                .nickname(getNickname())
                 .build();
     }
 
