777ea93cbc31f0c0b7b14be5c2647c445adab32e
langoustinee <xmun777@naver.com>
2024-02-16 15:56:24 +0900

    feat: 누락된 HttpStatus의 import static 구문 추가하기

--- src/main/java/com/kakao/saramaracommunity/member/exception/MemberErrorCode.java
+++ src/main/java/com/kakao/saramaracommunity/member/exception/MemberErrorCode.java
@@ -7,14 +7,15 @@ import com.kakao.saramaracommunity.common.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @RequiredArgsConstructor
 @Getter
 public enum MemberErrorCode implements ErrorCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다."),
+    MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 사용자입니다."),
+    WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 틀렸습니다."),
     UNAUTHORIZED_TO_MEMBER(FORBIDDEN, "권한이 없는 사용자입니다.");
 
     private final HttpStatus httpStatus;
