6b8b540b0d14891b4b1d63de91a2d0e4a91bf15f
IToriginal <gitshineit@gmail.com>
2024-02-17 19:07:32 +0900

    :recycle: Refactor: 단위 테스트, 통합 테스트 시나리오 기준 변경
    - 생성, 조회, 수정, 삭제를 기준으로 변경

--- src/test/java/com/kakao/saramaracommunity/comment/entity/CommentTest.java
+++ src/test/java/com/kakao/saramaracommunity/comment/entity/CommentTest.java
@@ -2,14 +2,17 @@ package com.kakao.saramaracommunity.comment.entity;
 
 import static com.kakao.saramaracommunity.fixture.BoardFixture.BOARD_VOTE_WRITER_LANGO;
 import static com.kakao.saramaracommunity.fixture.CommentFixture.COMMENT_WRITER_WOOGI;
+import static com.kakao.saramaracommunity.fixture.MemberFixture.NORMAL_MEMBER_SONNY;
 import static com.kakao.saramaracommunity.fixture.MemberFixture.NORMAL_MEMBER_WOOGI;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import com.kakao.saramaracommunity.board.entity.Board;
-import com.kakao.saramaracommunity.comment.exception.CommentBusinessException;
+import com.kakao.saramaracommunity.board.exception.BoardBusinessException;
 import com.kakao.saramaracommunity.member.entity.Member;
+import com.kakao.saramaracommunity.member.exception.MemberBusinessException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -19,100 +22,178 @@ import org.junit.jupiter.api.Test;
 /**
  * 댓글(Comment) 관련 도메인의 단위 테스트를 진행하는 테스트 클래스
  */
-@DisplayName("댓글은")
 class CommentTest {
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    class 회원_정보와_게시글_정보가_존재하는_경우 {
+    class 댓글_작성_시 {
 
-        @DisplayName("생성할 수 있다.")
-        @Test
-        void 생성할_수_있다() {
-            // given
-            Board REGISTED_BOARD = BOARD_VOTE_WRITER_LANGO.createBoard();
-            Member MEMBER_WOOGI = NORMAL_MEMBER_WOOGI.createMember();
-            String content = "1번 잠옷 농협은행!";
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보와_게시글_정보가_존재하는_경우 {
 
-            setField(REGISTED_BOARD, "id", 1L);
-            setField(MEMBER_WOOGI, "id", 2L);
+            private Member MEMBER_WOOGI;
+            private Board REGISTED_BOARD;
 
-            // when
-            Comment comment = Comment.of(MEMBER_WOOGI, REGISTED_BOARD, content);
+            @BeforeEach
+            void setUp() {
+                MEMBER_WOOGI = NORMAL_MEMBER_WOOGI.createMember();
+                REGISTED_BOARD = BOARD_VOTE_WRITER_LANGO.createBoard();
+                setField(REGISTED_BOARD, "id", 1L);
+                setField(MEMBER_WOOGI, "id", 2L);
+            }
 
-            // then
-            assertThat(comment.getMember()).isEqualTo(MEMBER_WOOGI);
-            assertThat(comment.getBoard()).isEqualTo(REGISTED_BOARD);
-            assertThat(comment.getContent()).isEqualTo(content);
+            @DisplayName("객체를 생성할 수 있다.")
+            @Test
+            void 객체를_생성할_수_있다() {
+                // given
+                String content = "1번 잠옷 농협은행!";
+
+                // when
+                Comment comment = Comment.of(MEMBER_WOOGI, REGISTED_BOARD, content);
+
+                // then
+                assertThat(comment.getMember()).isEqualTo(MEMBER_WOOGI);
+                assertThat(comment.getBoard()).isEqualTo(REGISTED_BOARD);
+                assertThat(comment.getContent()).isEqualTo(content);
+            }
+
         }
 
-        @DisplayName("수정할 수 있다.")
-        @Test
-        void 수정할_수_있다() {
-            // given
-            Comment REGISTED_COMMENT = COMMENT_WRITER_WOOGI.createComment();
-            Member MEMBER_WOOGI = COMMENT_WRITER_WOOGI.getWriter();
-            String MODIFY_CONTENT = "2번 잠옷 더 기업은행!";
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보가_존재하고_게시글_정보가_존재하지_않는_경우 {
 
-            // when
-            REGISTED_COMMENT.changeComment(MEMBER_WOOGI.getId(), MODIFY_CONTENT);
+            private Member MEMBER_WOOGI;
 
-            // then
-            assertThat(REGISTED_COMMENT.getContent()).isEqualTo(MODIFY_CONTENT);
+            @BeforeEach
+            void setUp() {
+                MEMBER_WOOGI = NORMAL_MEMBER_WOOGI.createMember();
+                setField(MEMBER_WOOGI, "id", 2L);
+            }
+
+            @DisplayName("객체를 생성할 수 없다.")
+            @Test
+            void 객체를_생성할_수_없다() {
+                // given
+                String content = "1번 잠옷 귀여워!!";
+
+                // when & then
+                assertThatThrownBy(() -> Comment.of(MEMBER_WOOGI, null, content))
+                        .isInstanceOf(BoardBusinessException.class)
+                        .hasMessage("게시글을 찾을 수 없습니다.");
+            }
+
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보가_존재하지_않고_게시글_정보는_존재하는_경우 {
+
+            private Board REGISTED_BOARD;
+
+            @BeforeEach
+            void setUp() {
+                REGISTED_BOARD = BOARD_VOTE_WRITER_LANGO.createBoard();
+                setField(REGISTED_BOARD, "id", 1L);
+            }
+
+            @DisplayName("객체를 생성할 수 없다.")
+            @Test
+            void 객체를_생성할_수_없다() {
+                // given
+                String content = "불법 광고 댓글";
+
+                // when & then
+                assertThatThrownBy(() -> Comment.of(null, REGISTED_BOARD, content))
+                        .isInstanceOf(MemberBusinessException.class)
+                        .hasMessage("존재하지 않는 사용자입니다.");
+            }
+
         }
 
     }
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    class 회원_정보가_존재하지_않으면 {
+    class 댓글_수정_시 {
 
-        @DisplayName("생성할 수 없고, MEMBER_NOT_FOUND 예외가 발생한다.")
-        @Test
-        void 생성할_수_없고_MEMBER_NOT_FOUND_예외가_발생한다() {
-            // given
-            Board REGISTED_BOARD = BOARD_VOTE_WRITER_LANGO.createBoard();
-            String content = "불법 광고 댓글";
+        private static Comment REGISTED_COMMENT;
 
-            setField(REGISTED_BOARD, "id", 1L);
-
-            // when & then
-            assertThatThrownBy(() -> Comment.of(null, REGISTED_BOARD, content))
-                    .isInstanceOf(CommentBusinessException.class);
+        @BeforeEach
+        void setUp() {
+            REGISTED_COMMENT = COMMENT_WRITER_WOOGI.createComment();
         }
 
-        @DisplayName("수정할 수 없고, MEMBER_NOT_FOUND 예외가 발생한다.")
-        @Test
-        void 수정할_수_없고_MEMBER_NOT_FOUND_예외가_발생한다() {
-            // given
-            Comment REGISTED_COMMENT = COMMENT_WRITER_WOOGI.createComment();
-            String MODIFY_CONTENT = "2번 잠옷 더 기업은행!";
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보와_댓글의_작성자가_일치하는_경우 {
 
-            // when & then
-            assertThatThrownBy(() -> REGISTED_COMMENT.changeComment(
-                    null, MODIFY_CONTENT
-                )
-            ).isInstanceOf(CommentBusinessException.class);
+            private Member MEMBER_WOOGI;
+
+            @BeforeEach
+            void setUp() {
+                MEMBER_WOOGI = COMMENT_WRITER_WOOGI.getWriter();
+            }
+
+            @DisplayName("수정할 수 있다.")
+            @Test
+            void 수정할_수_있다() {
+                // given
+                String MODIFY_CONTENT = "2번 잠옷 더 기업은행!";
+
+                // when
+                REGISTED_COMMENT.changeComment(MEMBER_WOOGI.getId(), MODIFY_CONTENT);
+
+                // then
+                assertThat(REGISTED_COMMENT.getContent()).isEqualTo(MODIFY_CONTENT);
+            }
+
         }
 
-    }
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보와_댓글의_작성자가_일치하지_않는_경우 {
 
-    @Nested
-    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    class 게시글_정보가_존재하지_않으면 {
+            private Member MEMBER_SONNY;
 
-        @DisplayName("생성할 수 없고, BOARD_NOT_FOUND 예외가 발생한다.")
-        @Test
-        void 생성할_수_없고_BOARD_NOT_FOUND_예외가_발생한다() {
-            // given
-            Member MEMBER_WOOGI = NORMAL_MEMBER_WOOGI.createMember();
-            String content = "1번 잠옷 귀여워!!";
+            @BeforeEach
+            void setUp() {
+                MEMBER_SONNY = NORMAL_MEMBER_SONNY.createMember();
+                setField(MEMBER_SONNY, "id", 3L);
+            }
 
-            setField(MEMBER_WOOGI, "id", 2L);
+            @DisplayName("수정할 수 없다.")
+            @Test
+            void 수정할_수_없다() {
+                // given
+                String MODIFY_CONTENT = "2번 잠옷 더 기업은행!";
 
-            // when & then
-            assertThatThrownBy(() -> Comment.of(MEMBER_WOOGI, null, content))
-                    .isInstanceOf(CommentBusinessException.class);
+                // when & then
+                assertThatThrownBy(
+                        () -> REGISTED_COMMENT.changeComment(MEMBER_SONNY.getId(), MODIFY_CONTENT))
+                        .isInstanceOf(MemberBusinessException.class)
+                        .hasMessage("권한이 없는 사용자입니다.");
+            }
+
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 회원_정보가_존재하지_않는_경우 {
+
+            @DisplayName("수정할 수 없다.")
+            @Test
+            void 수정할_수_없다() {
+                // given
+                String MODIFY_CONTENT = "2번 잠옷 더 기업은행!";
+
+                // when & then
+                assertThatThrownBy(() -> REGISTED_COMMENT.changeComment(null, MODIFY_CONTENT))
+                        .isInstanceOf(MemberBusinessException.class)
+                        .hasMessage("존재하지 않는 사용자입니다.");
+            }
+
         }
 
     }
