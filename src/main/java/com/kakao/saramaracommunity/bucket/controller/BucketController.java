877126c467ea95a25952ea6db09a5e3ceae682d5
langoustinee <xmun777@naver.com>
2024-02-20 14:03:59 +0900

    fix: API 응답 문구 수정하기

--- src/main/java/com/kakao/saramaracommunity/bucket/controller/BucketController.java
+++ src/main/java/com/kakao/saramaracommunity/bucket/controller/BucketController.java
@@ -1,10 +1,9 @@
 package com.kakao.saramaracommunity.bucket.controller;
 
+import com.kakao.saramaracommunity.bucket.dto.business.BucketCreateResponse;
 import com.kakao.saramaracommunity.bucket.service.BucketService;
-import com.kakao.saramaracommunity.bucket.service.dto.response.BucketCreateResponse;
 import com.kakao.saramaracommunity.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * 클라이언트의 요청을 받아 AWS S3 버킷에 이미지 업로드를 수행할 컨트롤러 클래스입니다.
@@ -34,7 +33,7 @@ public class BucketController {
         return ResponseEntity.ok().body(
                 ApiResponse.successResponse(
                         OK,
-                        "정상적으로 AWS S3 버킷에 이미지 등록을 완료했습니다.",
+                        "성공적으로 AWS S3 버킷에 이미지 업로드를 완료하였습니다.",
                         response
                 )
         );
