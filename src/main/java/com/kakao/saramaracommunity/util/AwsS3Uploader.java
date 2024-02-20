5a4bcc1f9e25ddd67aa8afcf51a4642328398e9d
langoustinee <xmun777@naver.com>
2024-02-20 14:07:08 +0900

    refactor: 유틸리티 패키지의 AwsS3Uploader 클래스 리팩토링하기
    1. void 메서드가 아닌 boolean 타입의 메서드를 두어 예외 처리함.
    2. validateFileExists 메서드명을 checkImageFileisEmpty로 수정함.

--- src/main/java/com/kakao/saramaracommunity/util/AwsS3Uploader.java
+++ src/main/java/com/kakao/saramaracommunity/util/AwsS3Uploader.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.kakao.saramaracommunity.bucket.exception.BucketBusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static com.kakao.saramaracommunity.bucket.exception.BucketErrorCode.BUCKET_AWS_S3_UPLOAD_FAILED;
+import static com.kakao.saramaracommunity.bucket.exception.BucketErrorCode.BUCKET_IMAGE_FILE_NOT_FOUND;
+
 /**
  * AWS S3 버킷에 파일 업로드 로직을 수행할 클래스입니다.
+ * 예외 발생시 BucketBusinessException을 발생시킵니다.
+ * 추후 단위 테스트 코드를 작성할 예정입니다.
  */
 @Log4j2
 @Component
@@ -27,47 +33,32 @@ public class AwsS3Uploader {
     private String bucketName;
 
     public String upload(MultipartFile file) {
-
-        boolean result = validateFileExists(file);
-
-        if(!result) {
+        if(checkImageFileisEmpty(file)) {
             log.info("[AwsS3Uploader] 업로드할 파일이 존재하지 않습니다.");
-            return null;
+            throw new BucketBusinessException(BUCKET_IMAGE_FILE_NOT_FOUND);
         }
-
         // 파일 명 생성하기
         String fileName = FileUtil.buildFileName(file.getOriginalFilename());
         log.info("[AwsS3Uploader] 업로드할 파일명: {}", fileName);
-
         // 파일 형식 설정
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(file.getContentType());
-
         // 파일의 내용을 읽어서 S3에 전송
         try (InputStream inputStream = file.getInputStream()) {
             log.info("[AwsS3Uploader] S3 버킷에 파일 업로드를 진행합니다.");
             amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (IOException e) {
-            log.info("[AwsS3Uploader] S3 버킷에 파일을 업로드 하던 중 문제가 발생했습니다.");
-            e.printStackTrace();
-            return null;
+            log.error("[AwsS3Uploader] S3 버킷에 파일을 업로드 하던 중 문제가 발생했습니다. 오류 정보 : {}", e.getMessage());
+            throw new BucketBusinessException(BUCKET_AWS_S3_UPLOAD_FAILED);
         }
-
         // S3 버킷에 업로드 된 이미지 파일의 실제 URL을 리턴
         String imagePath = amazonS3Client.getUrl(bucketName, fileName).toString();
         log.info("[AwsS3Uploader] S3 파일 업로드 성공했습니다, imgpath: {}", imagePath);
-
         return imagePath;
     }
 
-    /**
-     * validateFileExists: 업로드할 파일의 존재여부를 반환하는 메서드
-     *
-     * @param file
-     * @return boolean
-     */
-    private boolean validateFileExists(MultipartFile file) {
-        return !file.isEmpty();
+    private boolean checkImageFileisEmpty(MultipartFile file) {
+        return file.isEmpty();
     }
 
 }
