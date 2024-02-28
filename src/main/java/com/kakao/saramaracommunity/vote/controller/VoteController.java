c825bcc0f20a69e108ceb1d87e700e49451e2091
IToriginal <gitshineit@gmail.com>
2024-02-28 14:57:53 +0900

    :hammer: fix: 투표 조회시 권한 검증 로직 삭제

--- src/main/java/com/kakao/saramaracommunity/vote/controller/VoteController.java
+++ src/main/java/com/kakao/saramaracommunity/vote/controller/VoteController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,7 +52,6 @@ public class VoteController {
 
     /**
      * 투표 조회 API
-     * TODO: 사용자 권한 검증을 위한 보안성 강화 관련 개발 예정 (memberId)
      * @param boardId 투표 상태가 저장된 게시글 고유 식별자
      * @return "code", "message", "data" : { "boardId", "totalVotes", "voteCounts"}
      */
