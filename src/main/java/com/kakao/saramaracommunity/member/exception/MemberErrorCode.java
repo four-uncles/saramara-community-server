ec1882db5c35367d5396a3bfbc7763ba82830fbb
langoustinee <xmun777@naver.com>
2024-03-13 19:31:41 +0900

    fix: 불필요한 예외 코드 제거하기

--- src/main/java/com/kakao/saramaracommunity/member/exception/MemberErrorCode.java
+++ src/main/java/com/kakao/saramaracommunity/member/exception/MemberErrorCode.java
@@ -14,12 +14,9 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 @Getter
 public enum MemberErrorCode implements ErrorCode {
 
-    INVALID_EMAIL(BAD_REQUEST, "이메일 형식이 올바르지 않습니다."),
-    INVALID_NICKNAME(BAD_REQUEST, "닉네임은 최소 1자 이상, 최대 10자 이하까지 등록할 수 있습니다."),
-    INVALID_PASSWORD(BAD_REQUEST, "비밀번호는 최소 8자 이상, 최대 16자 이하로 숫자와 특수문자를 반드시 포함해야 합니다."),
-    WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 틀렸습니다."),
     MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 사용자입니다."),
-    UNAUTHORIZED_TO_MEMBER(FORBIDDEN, "권한이 없는 사용자입니다.");
+    UNAUTHORIZED_TO_MEMBER(FORBIDDEN, "권한이 없는 사용자입니다."),
+    WRONG_PASSWORD(BAD_REQUEST, "입력하신 비밀번호를 다시 확인해 주세요.");
 
     private final HttpStatus httpStatus;
 
