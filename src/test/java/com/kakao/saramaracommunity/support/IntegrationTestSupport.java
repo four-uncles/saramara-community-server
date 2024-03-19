16acf3129df3a29ce9811a233dbada566553472c
langoustinee <xmun777@naver.com>
2024-03-19 18:01:44 +0900

    feat: Testcontainers(테스트 컨테이너) 도입하기
    - 통합 테스트 클래스마다 MariaDB 컨테이너를 실행하도록 설정함.

--- src/test/java/com/kakao/saramaracommunity/support/IntegrationTestSupport.java
+++ src/test/java/com/kakao/saramaracommunity/support/IntegrationTestSupport.java
@@ -3,10 +3,15 @@ package com.kakao.saramaracommunity.support;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTestSupport {
-
+    @Container
+    static MariaDBContainer MARIADB_CONTAINER = new MariaDBContainer("mariadb:10.11");
 }
