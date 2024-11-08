e407eac7da8583cf39dc48acc820eae4fd23fa99
langoustinee <xmun777@naver.com>
2024-11-09 01:28:29 +0900

    remove: 불필요 import문 제거

--- src/test/java/com/kakao/saramaracommunity/architecture/GlobalArchitectureTest.java
+++ src/test/java/com/kakao/saramaracommunity/architecture/GlobalArchitectureTest.java
@@ -4,7 +4,6 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.library.Architectures;
 import com.tngtech.archunit.library.Architectures.LayeredArchitecture;
 import org.springframework.beans.factory.annotation.Autowired;
 
